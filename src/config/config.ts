dfb23bb3bd904fc76f2f98cdb71e6421612b5ace
HaDangQuang <hadangquang1408@gmail.com>
2025-11-10 21:14:14 +0700

    refactor: Config docker and db

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,21 +1,21 @@
 export default () => ({
   db: {
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '3306', 10),
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_DATABASE || 'cinehub',
-      synchronize: true,
-    },
+    type: 'mysql',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'cinehub',
+    synchronize: true,
+  },
   jwt: {
     access: {
       secret: process.env.JWT_ACCESS_SECRET || '',
-      expired: process.env.JWT_ACCESS_EXPIRED || ''
+      expired: process.env.JWT_ACCESS_EXPIRED || '',
     },
     refresh: {
       secret: process.env.JWT_REFRESH_SECRET || '',
-      expired: process.env.JWT_REFRESH_EXPRIED || ''
-    }
-  }
+      expired: process.env.JWT_REFRESH_EXPRIED || '',
+    },
+  },
 });
